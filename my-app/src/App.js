4a2edb50e632005f456a3d794b8d0f9b51a5cecd
semetea <55260856+semetea@users.noreply.github.com>
2022-08-02 14:18:32 -0400

    useTabs

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,31 +1,38 @@
 import './App.css';
 import { useState } from "react";
 
-const useInput = (initialValue, validator) => {
-  const [value, setValue] = useState(initialValue);
-  const onChange = event => {
-    const {
-      target: { value }
-    } = event;
-    let willUpdate = true;
-    if (typeof (validator) == 'function') {
-      willUpdate = validator(value)
-    }
-    if (willUpdate) {
-      setValue(value)
-    }
+const content = [
+  {
+    tab: "Section 1",
+    content: "I'm the content of the Section 1"
+  },
+  {
+    tab: "Section 2",
+    content: "I'm the content of the Section 2"
   }
-  return { value, onChange };
-}
+]
+
+const useTabs = (initialTab, allTabs) => {
+  const [currentIndex, setCurrentIndex] = useState(initialTab)
+  if (!allTabs || !Array.isArray(allTabs)) {
+    return;
+  }
+  return {
+    currentItem: allTabs[currentIndex],
+    changeItem: setCurrentIndex
+  };
+};
 
 function App() {
-  const maxLen = (value) => !value.includes("@")
-  const name = useInput("Mr.", maxLen)
+  const { currentItem, changeItem } = useTabs(0, content);
   return (
     <div className="App">
-      <div>Hello</div>
-      <input placeholder='Name' {...name} />
-    </div>
+      {content.map((section, index) => (
+        <button onClick={() => changeItem(index)}> {section.tab}</button>
+      ))
+      }
+      <div>{currentItem.content}</div>
+    </div >
   );
 }
 
