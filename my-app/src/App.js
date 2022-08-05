b63e59e4b118282b66c0435b56cb5f8e8310df58
semetea <55260856+semetea@users.noreply.github.com>
2022-08-05 14:37:05 -0400

    useClick

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,27 @@
 import './App.css';
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
-const useTitle = (initialTitle) => {
-  const [title, setTitle] = useState(initialTitle)
-  const updateTitle = () => {
-    const htmlTitle = document.querySelector("title")
-    htmlTitle.innerText = title;
-  }
-  useEffect(updateTitle, [title])
-  return setTitle;
+const useClick = (onClick) => {
+  const element = useRef();
+  useEffect(()=> {
+    if(element.current){
+      element.current.addEventListener("click", onClick);
+    }
+    return () => {
+      if(element.current) {
+        element.current.removeEventListener("click", onClick);
+      }
+    }
+  }, [])
+  return element;
 }
 
-function App() {
-  const titleUpdater = useTitle("Loading...")
-  setTimeout(() => titleUpdater("Home"), 5000)
+const App = () => {
+  const sayHello = () => console.log("say hello")
+  const title = useClick(sayHello);
   return (
     <div className='App'>
+      <h1 ref={title}>Hi</h1>
     </div>
   );
 }
